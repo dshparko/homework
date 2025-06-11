f91a8092f826a09daa456bb62279e7f7d99035bd
dshparko <951008d@mail.ru>
2025-06-11 20:21:36 +0300

    feat: realize hashmap

---
+++ HashMap/src/Node.java
@@ -0,0 +1,27 @@
+public class Node<K, V> {
+    private final int hash;
+    private final K key;
+    private V value;
+
+    Node(int hash, K key, V value) {
+        this.hash = hash;
+        this.key = key;
+        this.value = value;
+    }
+
+    public int getHash() {
+        return hash;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}
