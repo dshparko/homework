54a70700521f5e74600e2ef4943c487059dc540f
dshparko <951008d@mail.ru>
2025-06-15 20:22:47 +0300

    feat: create userDAO interface with necessary methods

---
+++ UserService/src/main/java/UserDAO.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public interface UserDAO {
+    void create(User user);
+    User read(Long id);
+    void update(User user);
+    void delete(Long id);
+    List<User> findAll();
+}
