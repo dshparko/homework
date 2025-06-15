8173e7e3956723021d5aa27dbdeb4188d48ba2b3
dshparko <951008d@mail.ru>
2025-06-15 21:26:07 +0300

    feat: implement methods from interface UserDAO

---
+++ UserService/src/main/java/UserDAOImpl.java
@@ -0,0 +1,72 @@
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class UserDAOImpl implements UserDAO {
+
+    private void executeInsideTransaction(Consumer<Session> action) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            action.accept(session);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.err.println("Transaction was failed: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void create(User user) {
+        executeInsideTransaction(session -> {
+            session.persist(user);
+            System.out.println("User was created: " + user);
+        });
+    }
+
+    @Override
+    public User read(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.find(User.class, id);
+        } catch (Exception e) {
+            System.err.println("Error reading user: " + e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public void update(User user) {
+        executeInsideTransaction(session -> {
+            session.merge(user);
+            System.out.println("User was updated: " + user);
+        });
+    }
+
+    @Override
+    public void delete(Long id) {
+        executeInsideTransaction(session -> {
+            User user = session.find(User.class, id);
+            if (user != null) {
+                session.remove(user);
+                System.out.println("User was deleted: " + user);
+            } else {
+                System.out.println("User wasn't found: id=" + id);
+            }
+        });
+    }
+
+    @Override
+    public List<User> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM User", User.class).list();
+        } catch (Exception e) {
+            System.err.println("Error retrieving users: " + e.getMessage());
+            return Collections.emptyList();
+        }
+    }
+}
