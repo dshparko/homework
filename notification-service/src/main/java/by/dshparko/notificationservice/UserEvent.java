2e810770148137b1bdabd3896976fdff589ebb88
dshparko <951008d@mail.ru>
2025-07-10 23:00:49 +0300

    feat: add constructor

--- notification-service/src/main/java/by/dshparko/notificationservice/UserEvent.java
+++ notification-service/src/main/java/by/dshparko/notificationservice/UserEvent.java
@@ -1,9 +1,11 @@
 package by.dshparko.notificationservice;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@AllArgsConstructor
 @Data
 public class UserEvent {
     private String type;
