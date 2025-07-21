8bdf658b58e6ba82227753054fbf14f91ac9e9f4
dshparko <951008d@mail.ru>
2025-07-21 19:03:41 +0300

    feat: add description for swagger

---
+++ UserService/src/main/java/dshparko/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package dshparko.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI userServiceOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Service API")
+                        .description("REST API for managing users")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Developer")
+                                .url("https://github.com/dshparko"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")));
+    }
+}
--- UserService/src/main/java/dshparko/controller/UserController.java
+++ UserService/src/main/java/dshparko/controller/UserController.java
@@ -1,6 +1,7 @@
 package dshparko.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
@@ -13,14 +14,25 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import dshparko.dto.UserDto;
 import dshparko.service.UserService;
+
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Tag(name = "User Controller", description = "Operations for managing users: creating, receiving, updating and deleting")
 public class UserController {
     private final UserService service;
 
+    @Operation(summary = "Get a list of all users", description = "Returns a collection of users with HATEOAS links")
+    @ApiResponse(responseCode = "200", description = "User list successfully returned")
     @GetMapping
     public CollectionModel<EntityModel<UserDto>> all() {
         List<EntityModel<UserDto>> users = service.findAllUsers().stream()
@@ -38,8 +50,15 @@ public class UserController {
         return CollectionModel.of(users, linkTo(methodOn(UserController.class).all()).withSelfRel());
     }
 
+    @Operation(summary = "Get user by ID", description = "Returns one user with HATEOAS links")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User was found"),
+            @ApiResponse(responseCode = "404", description = "User wasn't found")
+    })
     @GetMapping("/{id}")
-    public EntityModel<UserDto> byId(@PathVariable("id") Long id) throws JsonProcessingException {
+    public EntityModel<UserDto> byId(@PathVariable("id")
+                                     @Parameter(description = "ID of the user to retrieve", example = "1")
+                                     Long id) throws JsonProcessingException {
         UserDto dto = service.getUser(id);
 
         return EntityModel.of(dto,
@@ -49,8 +68,16 @@ public class UserController {
                 linkTo(methodOn(UserController.class).all()).withRel("all-users"));
     }
 
+    @Operation(summary = "Create a new user", description = "Accepts DTO and creates user")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "User was created successfully"),
+            @ApiResponse(responseCode = "400", description = "Validation error")
+    })
     @PostMapping
-    public ResponseEntity<EntityModel<UserDto>> create(@RequestBody @Validated UserDto dto) throws JsonProcessingException {
+    public ResponseEntity<EntityModel<UserDto>> create(@RequestBody(
+            description = "User data: \"name\" - Username (required), \"email\" - User email (valid), \"age\" - User age (>= 0)"
+    )
+                                                       @Validated UserDto dto) throws JsonProcessingException {
         UserDto created = service.createUser(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 EntityModel.of(created,
@@ -59,6 +86,11 @@ public class UserController {
         );
     }
 
+    @Operation(summary = "Refresh user", description = "Updates existing user by ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User was successfully updated"),
+            @ApiResponse(responseCode = "404", description = "User wasn't found")
+    })
     @PutMapping("/{id}")
     public EntityModel<UserDto> update(@PathVariable("id") Long id, @RequestBody @Validated UserDto dto) throws JsonProcessingException {
         UserDto updated = service.updateUser(id, dto);
@@ -67,8 +99,15 @@ public class UserController {
                 linkTo(methodOn(UserController.class).all()).withRel("all-users"));
     }
 
+    @Operation(summary = "Delete a user", description = "Remove user by ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "User was successfully removed"),
+            @ApiResponse(responseCode = "404", description = "User wasn't found")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable("id") Long id) throws JsonProcessingException {
+    public ResponseEntity<Void> delete(@PathVariable("id")
+                                       @Parameter(description = "ID of the user to delete", example = "1")
+                                       Long id) throws JsonProcessingException {
         service.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
