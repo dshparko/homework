159848fd4e3f73eaae0bdb65f6cda540ef73b1c4
dshparko <951008d@mail.ru>
2025-07-24 21:29:08 +0300

    feat: add service discovery pattern

--- UserService/src/main/java/dshparko/App.java
+++ UserService/src/main/java/dshparko/App.java
@@ -3,9 +3,11 @@ package dshparko;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableDiscoveryClient
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
