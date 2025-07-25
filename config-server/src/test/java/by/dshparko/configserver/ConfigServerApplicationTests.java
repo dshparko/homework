c745598b8fd30d477d5e6ef93f6e743b5b9c3be7
dshparko <951008d@mail.ru>
2025-07-25 15:43:10 +0300

    feat: add config server

---
+++ config-server/src/test/java/by/dshparko/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dshparko.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
